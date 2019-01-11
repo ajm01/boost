0122e26a3238656b6072ad0824ac9e01c5f9aab0
Andrew Mauer <ajmauer@us.ibm.com>
2019-01-11 12:55:31 -0500

    microprofile support in sync with jdbc booster support

--- boost-common/src/main/java/io/openliberty/boost/common/utils/LibertyBoosterUtil.java
+++ boost-common/src/main/java/io/openliberty/boost/common/utils/LibertyBoosterUtil.java
@@ -16,57 +16,124 @@ import java.util.Map;
 
 import io.openliberty.boost.common.BoostLoggerI;
 import io.openliberty.boost.common.config.BoosterPackConfigurator;
+import io.openliberty.boost.common.config.CDIBoosterPackConfigurator;
 import io.openliberty.boost.common.config.JAXRSBoosterPackConfigurator;
 import io.openliberty.boost.common.config.JDBCBoosterPackConfigurator;
+import io.openliberty.boost.common.config.JSONPBoosterPackConfigurator;
 import io.openliberty.boost.common.config.LibertyServerConfigGenerator;
+import io.openliberty.boost.common.config.MPConfigBoosterPackConfigurator;
+import io.openliberty.boost.common.config.MPHealthBoosterPackConfigurator;
+import io.openliberty.boost.common.config.MPOpenTracingBoosterPackConfigurator;
+import io.openliberty.boost.common.config.MPRestClientBoosterPackConfigurator;
 
 public class LibertyBoosterUtil {
 
     public static String BOOSTER_JAXRS = "io.openliberty.boosters:jaxrs";
     public static String BOOSTER_JDBC = "io.openliberty.boosters:jdbc";
-    
+
     public static String DERBY_DEPENDENCY = "org.apache.derby:derby";
+    public String BOOSTERS_GROUP_ID = "io.openliberty.boosters";
+
+    public static String BOOSTER_MPHEALTH = "io.openliberty.boosters:mpHealth";
+    public static String BOOSTER_JSONP = "io.openliberty.boosters:jsonp";
+    public static String BOOSTER_CDI = "io.openliberty.boosters:cdi";
+    public static String BOOSTER_MPCONFIG = "io.openliberty.boosters:mpConfig";
+    public static String BOOSTER_MPRESTCLIENT = "io.openliberty.boosters:mpRestClient";
+    public static String BOOSTER_OPENTRACING = "io.openliberty.boosters:mpOpenTracing";
+
+    protected String libertyServerPath;
+    protected List<BoosterPackConfigurator> boosterPackConfigurators;
+    protected BoostLoggerI logger;
+
+    public LibertyBoosterUtil(String libertyServerPath, Map<String, String> dependencies, BoostLoggerI logger) {
+        this.libertyServerPath = libertyServerPath;
+        this.logger = logger;
+
+        this.boosterPackConfigurators = getBoosterPackConfigurators(dependencies);
+    }
 
     /**
-     * take a list of pom boost dependency strings and map to liberty features for
-     * config. return a list of feature configuration objects for each found
+     * take a list of pom boost dependency strings and map to liberty features
+     * for config. return a list of feature configuration objects for each found
      * dependency.
      * 
      * @param dependencies
      * @return
      */
     public static List<BoosterPackConfigurator> getBoosterPackConfigurators(Map<String, String> dependencies) {
-    	
-    	List<BoosterPackConfigurator> boosterPackConfigList = new ArrayList<BoosterPackConfigurator>();
-    	
+
+        List<BoosterPackConfigurator> boosterPackConfigList = new ArrayList<BoosterPackConfigurator>();
+
         if (dependencies.containsKey(BOOSTER_JDBC)) {
-        	
+
             JDBCBoosterPackConfigurator jdbcConfig = new JDBCBoosterPackConfigurator();
-        	
+
             String version = dependencies.get(BOOSTER_JDBC);
             jdbcConfig.setFeature(version);
-            
+
             if (dependencies.containsKey(DERBY_DEPENDENCY)) {
-            	String derbyVersion = dependencies.get(DERBY_DEPENDENCY);
-            	jdbcConfig.setDependency(DERBY_DEPENDENCY + ":" + derbyVersion);
+                String derbyVersion = dependencies.get(DERBY_DEPENDENCY);
+                jdbcConfig.setDependency(DERBY_DEPENDENCY + ":" + derbyVersion);
             }
             boosterPackConfigList.add(jdbcConfig);
-            
+
         }
-        if (dependencies.containsKey(BOOSTER_JAXRS)) {		
+        if (dependencies.containsKey(BOOSTER_JAXRS)) {
             JAXRSBoosterPackConfigurator jaxrsConfig = new JAXRSBoosterPackConfigurator();
-            
+
             String version = dependencies.get(BOOSTER_JAXRS);
             jaxrsConfig.setFeature(version);
-            
+
             boosterPackConfigList.add(jaxrsConfig);
         }
+        if (dependencies.containsKey(BOOSTER_MPHEALTH)) {
+            MPHealthBoosterPackConfigurator mpHealthConfig = new MPHealthBoosterPackConfigurator();
+            String version = dependencies.get(BOOSTER_MPHEALTH);
+            mpHealthConfig.setFeature(version);
 
+            boosterPackConfigList.add(mpHealthConfig);
+        }
+        if (dependencies.containsKey(BOOSTER_MPCONFIG)) {
+            MPConfigBoosterPackConfigurator mpConfigConfig = new MPConfigBoosterPackConfigurator();
+            String version = dependencies.get(BOOSTER_MPCONFIG);
+            mpConfigConfig.setFeature(version);
+
+            boosterPackConfigList.add(mpConfigConfig);
+        }
+        if (dependencies.containsKey(BOOSTER_CDI)) {
+            CDIBoosterPackConfigurator CDIConfig = new CDIBoosterPackConfigurator();
+            String version = dependencies.get(BOOSTER_CDI);
+            CDIConfig.setFeature(version);
+
+            boosterPackConfigList.add(CDIConfig);
+        }
+        if (dependencies.containsKey(BOOSTER_MPRESTCLIENT)) {
+            MPRestClientBoosterPackConfigurator mpRestClientConfig = new MPRestClientBoosterPackConfigurator();
+            String version = dependencies.get(BOOSTER_MPRESTCLIENT);
+            mpRestClientConfig.setFeature(version);
+
+            boosterPackConfigList.add(mpRestClientConfig);
+        }
+        if (dependencies.containsKey(BOOSTER_JSONP)) {
+            JSONPBoosterPackConfigurator jsonpConfig = new JSONPBoosterPackConfigurator();
+            String version = dependencies.get(BOOSTER_JSONP);
+            jsonpConfig.setFeature(version);
+
+            boosterPackConfigList.add(jsonpConfig);
+        }
+        if (dependencies.containsKey(BOOSTER_OPENTRACING)) {
+            MPOpenTracingBoosterPackConfigurator mpOpenTracingConfig = new MPOpenTracingBoosterPackConfigurator();
+            String version = dependencies.get(BOOSTER_OPENTRACING);
+            mpOpenTracingConfig.setFeature(version);
+
+            boosterPackConfigList.add(mpOpenTracingConfig);
+        }
 
         return boosterPackConfigList;
     }
 
-    public static void generateLibertyServerConfig(String libertyServerPath, List<BoosterPackConfigurator> boosterPackConfigurators, String warName) throws Exception {
+    public static void generateLibertyServerConfig(String libertyServerPath,
+            List<BoosterPackConfigurator> boosterPackConfigurators, String warName) throws Exception {
 
         LibertyServerConfigGenerator serverConfig = new LibertyServerConfigGenerator(libertyServerPath);
 
@@ -88,19 +155,20 @@ public class LibertyBoosterUtil {
         serverConfig.writeToServer();
     }
 
-    public static List<String> getDependenciesToCopy(List<BoosterPackConfigurator> boosterPackConfigurators, BoostLoggerI logger) {
-    	
-    	List<String> dependenciesToCopy = new ArrayList<String>();
-    	
-    	for (BoosterPackConfigurator configurator : boosterPackConfigurators) {
-    		String dependencyToCopy = configurator.getDependency();
-    		
-    		if (dependencyToCopy != null) {
-            	logger.info(dependencyToCopy);
-    			dependenciesToCopy.add(dependencyToCopy);
-    		}
-    	}
-    	
-    	return dependenciesToCopy;
+    public static List<String> getDependenciesToCopy(List<BoosterPackConfigurator> boosterPackConfigurators,
+            BoostLoggerI logger) {
+
+        List<String> dependenciesToCopy = new ArrayList<String>();
+
+        for (BoosterPackConfigurator configurator : boosterPackConfigurators) {
+            String dependencyToCopy = configurator.getDependency();
+
+            if (dependencyToCopy != null) {
+                logger.info(dependencyToCopy);
+                dependenciesToCopy.add(dependencyToCopy);
+            }
+        }
+
+        return dependenciesToCopy;
     }
 }
